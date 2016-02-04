b049277f38a7287120b6d98172dd5d8892545c96
linwei365 <linwei365@gmail.com>
2016-02-03 23:04:06 -0800

    created dev branch

---
+++ DelegationInSwiftExample/SecondViewController.swift
@@ -0,0 +1,46 @@
+//
+//  SecondViewController.swift
+//  DelegationInSwiftExample
+//
+//  Created by Lin Wei on 2/3/16.
+//  Copyright © 2016 Lin Wei. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: ViewController {
+
+    @IBOutlet weak var textLabel: UILabel!
+    @IBOutlet weak var textField: UITextField!
+    var text:String = ""
+    
+    @IBAction func goBackButtonOnClick(sender: UIButton) {
+        
+        presentingViewController?.dismissViewControllerAnimated(true, completion: nil)
+        
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- DelegationInSwiftExample/ViewController.swift
+++ DelegationInSwiftExample/ViewController.swift
@@ -10,11 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+ 
+    
+    @IBOutlet weak var textLabelA: UILabel!
+    
+    @IBOutlet weak var textFieldA: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
